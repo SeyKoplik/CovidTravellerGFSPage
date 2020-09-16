5a77725425bc57c3552c73a66bec33776da18f6d
Seychelle Koplik <sey.koplik@gmail.com>
2020-09-16 19:16:28 -0400

    search button optimization

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-$("#covidButton").on("click", function() {
-$('.ui.modal')
-  .modal('show');
+$("#covidButton").on("click", function() {  
+    $('.ui.modal')
+        .modal('show');
 });
+
+
+$("#searchIcon").on("click", function() {
+    $(".showAllStateFacts").show();
+});
+
+var googleAPIKey = "AIzaSyBmtloOP1ulwJc773TYUNTOXo1n-QncgFo"
