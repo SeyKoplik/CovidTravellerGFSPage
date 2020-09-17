fb0f509da33cb8b03916c2aa223a2bf81bd4b4fb
Geovany Cardoza <geoyd17@gmail.com>
2020-09-17 13:06:47 -0400

    finish testing the chart

--- script.js
+++ script.js
@@ -38,50 +38,44 @@ var config = {
 
 // var queryURL = "https://api.covidtracking.com/v1/states/info.json";
 
-function loadDoc() {
-  var state = $("#states").val();
-  var url = "https://api.covidtracking.com/v1/states/current.json" + state;
-  console.log(state);
-}
-
+// function loadDoc() {
+//   var state = $("#states").val();
+//   var url = "https://api.covidtracking.com/v1/states/current.json" + state;
+//   console.log(state);
+// }
+var statesArr = {};
 var statesInfo;
 var queryURL = "https://api.covidtracking.com/v1/states/current.json";
 
 $(document).ready(function () {
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    var results = response;
-    // clear pieChartData
-    // pieChartData = [];
-    var states = "PA";
-    for (var i = 0; i < results.length; i++) {
-      if (states == results[i].state) {
-        var positiveCases = results[i].positive;
-        var negativeCases = results[i].negative;
-        var deaths = results[i].death;
-        var hospitalizedNow = results[i].hospitalizedCurrently;
-        var recoveredNum = results[i].recovered;
-        console.log(results);
-        // alert(negativeCases);
-        pieChartData.push(positiveCases);
-        pieChartData.push(negativeCases);
-        pieChartData.push(deaths);
-        pieChartData.push(hospitalizedNow);
-        pieChartData.push(recoveredNum);
-        var ctx = document.getElementById("chart-area-pie").getContext("2d");
-        window.myPie = new Chart(ctx, config);
-        // window.myPie.update();
+  function ajaxCallForCovid(stateIDForAPI) {
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      var results = response;
+      // clear pieChartData
+      // pieChartData = [];
+      var states = stateIDForAPI;
+      for (var i = 0; i < results.length; i++) {
+        if (states == results[i].state) {
+          var positiveCases = results[i].positive;
+          var negativeCases = results[i].negative;
+          var deaths = results[i].death;
+          var hospitalizedNow = results[i].hospitalizedCurrently;
+          var recoveredNum = results[i].recovered;
+          console.log(results);
+          // alert(negativeCases);
+          pieChartData.push(positiveCases);
+          pieChartData.push(negativeCases);
+          pieChartData.push(deaths);
+          pieChartData.push(hospitalizedNow);
+          pieChartData.push(recoveredNum);
+          var ctx = document.getElementById("chart-area-pie").getContext("2d");
+          window.myPie = new Chart(ctx, config);
+          // window.myPie.update();
+        }
       }
-    }
-  });
+    });
+  }
 });
-
-//   $.ajax({
-//     url: queryURL,
-//     type: "Get",
-//   }).then(function (response) {
-//     console.log(response);
-//   });
-// });
