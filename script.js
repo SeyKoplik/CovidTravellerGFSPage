4bced3acc2d6ad0fa3b0d76294e43e38670eeccd
Seychelle Koplik <sey.koplik@gmail.com>
2020-09-16 21:58:13 -0400

    adjusted display boxes

--- script.js
+++ script.js
@@ -8,4 +8,8 @@ $("#searchIcon").on("click", function() {
     $(".showAllStateFacts").show();
 });
 
-var googleAPIKey = "AIzaSyBmtloOP1ulwJc773TYUNTOXo1n-QncgFo"
+var googleAPIKey = "AIzaSyBmtloOP1ulwJc773TYUNTOXo1n-QncgFo";
+
+var triposoAccount = "BD0IOKOY";
+
+var triposo = "mvo5l46vms96lmtmgvhmj9gztbc138fi"
