67919e103daa197859bad8c0646ec4b667e4d268
Seychelle Koplik <sey.koplik@gmail.com>
2020-09-18 12:26:38 -0400

    cleaned up code for chart

--- script.js
+++ script.js
@@ -52,7 +52,6 @@ $(document).ready(function () { //===BEGINNING OF ALL PAGE
             url: queryURL,
             method: "GET"
         }).then(function (stateData) {
-            console.log("ajaxCallStateData");
             var stateSnip = stateData.results[1].snippet;
             $("#stateName").empty();
             $("#stateName").append(newStateInput);
@@ -61,8 +60,8 @@ $(document).ready(function () { //===BEGINNING OF ALL PAGE
             var stateImage = stateData.results[1].images[1].source_url;
             $(".tinyImageGoesHere").attr("src", stateImage);
             $(".tinyImageGoesHere").attr("width", "150px");
-        }) 
-    } 
+        })
+    }
 
     function displayStatePic(newStateInput) {
         var apiKey = "xFUVUkBbo_C02js7Z6F1qftvkYa-c2GTQfHNQ_B7mJk"
@@ -71,7 +70,6 @@ $(document).ready(function () { //===BEGINNING OF ALL PAGE
             url: queryUrl,
             method: "GET"
         }).then(function (response) {
-            console.log("displayStatePic")
             $(".bigImage1").attr('src', response.results[0].urls.thumb);
             $(".bigImage2").attr('src', response.results[1].urls.thumb);
             $(".bigImage3").attr('src', response.results[2].urls.thumb);
@@ -81,75 +79,83 @@ $(document).ready(function () { //===BEGINNING OF ALL PAGE
 
     $(document).on("click", ".state-link", function () {
         var savedState = $(this).attr("data-name");
+        console.log(savedState);
+        pieChartData = [];
         ajaxCallStateData(savedState);
         displayStatePic(savedState);
-        ajaxCallForCovid(convertedState(savedState));
+        ajaxCallForCovid(convertState(savedState));
     })
 
     function ajaxCallForCovid(stateIDForAPI) {
         var queryURL = "https://api.covidtracking.com/v1/states/current.json";
-
         $.ajax({
             url: queryURL,
             method: "GET",
         }).then(function (response) {
             var results = response;
             var states = stateIDForAPI;
+
             for (var i = 0; i < results.length; i++) {
                 if (states == results[i].state) {
+                    console.log(i);
+                    console.log(states);
+                    console.log(results[i].state);
                     var positiveCases = results[i].positive;
                     var negativeCases = results[i].negative;
                     var deaths = results[i].death;
                     var hospitalizedNow = results[i].hospitalizedCurrently;
                     var recoveredNum = results[i].recovered;
 
+                    var pieChartData = [];
+
                     pieChartData.push(positiveCases);
                     pieChartData.push(negativeCases);
                     pieChartData.push(deaths);
                     pieChartData.push(hospitalizedNow);
                     pieChartData.push(recoveredNum);
-                    var ctx = document.getElementById("chart-area-pie").getContext("2d");
-                    window.myPie = new Chart(ctx, config);
 
+                    //PIE CHART CONFIG
+                    var config = {
+                        type: "pie",
+                        data: {
+                            datasets: [
+                                {
+                                    data: pieChartData,
+                                    backgroundColor: [
+                                        window.chartColors.red,
+                                        window.chartColors.green,
+                                        window.chartColors.black,
+                                        window.chartColors.orange,
+                                        window.chartColors.purple,
+                                    ],
+                                    label: "Dataset 1",
+                                },
+                            ],
+                            labels: [
+                                "Positive Cases",
+                                "Negative Cases",
+                                "Deaths",
+                                "Hospitalized Currently",
+                                "Recovered",
+                            ],
+                        },
+                        options: {
+                            responsive: true,
+                            title: {
+                                display: true,
+                                text: "Covid-19 Chart",
+                            },
+                        },
+                    };
+
+                    var ctx = document.getElementById("chart-area-pie").getContext("2d");
+                    console.log(window.myPie);
+                    window.myPie;
+                    window.myPie = new Chart(ctx, config);
+                    window.myPie.update();
                 }
             }
         });
     }
-    //PIE CHART DATA
-    var pieChartData = [];
-
-    //PIE CHART CONFIG
-    var config = {
-        type: "pie",
-        data: {
-            datasets: [
-                {
-                    data: pieChartData,
-                    backgroundColor: [
-                        window.chartColors.red,
-                        window.chartColors.green,
-                        window.chartColors.black,
-                        window.chartColors.orange,
-                        window.chartColors.purple,
-                    ],
-                    label: "Dataset 1",
-                },
-            ],
-            labels: [
-                "Positive Cases",
-                "Negative Cases",
-                "Deaths",
-                "Hospitalized Currently",
-                "Recovered",
-            ],
-        },
-        options: {
-            responsive: true,
-            title: {
-                display: true,
-                text: "Covid-19 Chart",
-            },
-        },
-    };
 
 }); //=== END OF ALL PAGE
